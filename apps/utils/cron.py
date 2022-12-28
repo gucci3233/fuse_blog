bf5b65f06121055b23dc0bfd39fb683507af8b4c
adkahmjon <adkhamjonmakmudov@gmail.com>
2022-12-28 22:32:40 +0500

    Initial commit

---
+++ apps/utils/cron.py
@@ -0,0 +1,18 @@
+import datetime
+import os
+
+import django
+
+from apps.models import Post
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "root.settings")
+django.setup()
+
+
+def cron_job():
+    date = datetime.date.today()
+    date_delta = datetime.timedelta(7)
+    # Post.objects.filter(Q(created_at__lt=date - date_delta), Q(status=Post.Status.CANCEL)).delete()
+    s = Post.objects.get(pk=2)
+    s.title = 'ssss'
+    s.save()
